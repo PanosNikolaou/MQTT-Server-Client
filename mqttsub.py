9e68ad888057f2bb55812ee4363ea8adf9e5f3ff
Panos Nikolaou <38185286+PanosNikolaou@users.noreply.github.com>
2018-04-09 17:02:18 +0200

    Update mqttsub.py

--- mqttsub.py
+++ mqttsub.py
@@ -8,15 +8,6 @@ import numpy as np
 import pandas as pd
 pd.set_option('max_columns', 50)
 
-#connection = pymysql.connect(host='localhost',
-#                             user='root',
-#                             password='',
-#
-#                             db='colibri',
-#                             autocommit=True,
-#                             charset='utf8mb4',
-#                             cursorclass=pymysql.cursors.DictCursor)
-
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
     print("CONNACK received with code %d." % (rc))
@@ -28,10 +19,6 @@ def on_connect(client, userdata, rc):
 def on_message(client, userdata, msg):
     if msg.payload:
         print(msg.payload)
-        #datalist =  list(msg.payload)
-        #print(datalist.pop(0))
-        #print(datalist.pop(1))
-        #print(datalist.pop(2))
     else:
         print("empty message")
 
@@ -47,4 +34,3 @@ client.on_subscribe = on_subscribe
 client.connect("broker.hivemq.com", 1883, 0)
 
 client.loop_forever()
-
